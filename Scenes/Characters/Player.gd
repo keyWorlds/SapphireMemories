6669d94e64560c28fd7ebb5fc7f71ff8c25a66e8
Elena G <elena.guzbla@gmail.com>
2020-04-23 20:59:22 +0200

    Add physics to player movement

--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const ACC = 10
+const MAX_SPEED = 100
+const FRICTION = 10
+
 var vel = Vector2.ZERO
 
 func _physics_process(delta):
@@ -7,10 +11,12 @@ func _physics_process(delta):
 	
 	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input = input.normalized()
 	
 	if input != Vector2.ZERO:
-		vel = input
+		vel += input * ACC * delta
+		vel = vel.clamped(MAX_SPEED * delta)
 	else:
-		vel = Vector2.ZERO
+		vel = vel.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move_and_collide(vel)
